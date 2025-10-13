13f4a27bd91e7e342c00a8b2de30afecf5495c74
ste-lia <stefoglolia9@gmail.com>
2025-10-13 15:00:49 +0300

    Variabile, tipuri de date.

--- script.js
+++ script.js
@@ -5,7 +5,51 @@ let activitate = 'Cursant FrontEndDev';
 console.log(prenume);
 console.log(activitate);
 
-let a = '5';
-let b = '2';
+// let a = '5';
+// let b = '2';
 
-console.log(a + b);
+// console.log(a + b);
+
+// tipuri de date 
+
+// 1.string - sir de caractere(litere,cifre...)
+// let nr1 = '7';
+
+// 2.number - numere (operatii matematice)
+// let nr2 = 7;
+
+let a = 5;
+let b = 7;
+
+console.log(prenume + a);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// 3.boolean (true or false)
+
+let passwordMatches = true;
+
+// 4.null - lipsa informatiei
+
+// 5.undefined - lipsa variabilei (error)
+
+// typeof - arata tipul de date a variabilei
+
+// console.log(passwordMatches)
+// in consola true
+
+console.log(typeof passwordMatches);
+// in consola boolean
+
+prenume = 'Ion';
+console.log(prenume);
+// in consola va fi Ion
+
+// const - valoarea variabilea nu mai poate fi modificata
+// numele se scrie cu majuscule
+
+const API_KEY = 'abvgd12345oparola';
+
+// API_KEY = 'schimb';
+// dupa care va fi error si nu a sa mai lucreze
